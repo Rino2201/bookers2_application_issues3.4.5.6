e3ba5878ce6d70d1cda66860fc3f09acde4512e1
NoriakiNakamura <babababargxx@gmail.com>
2022-02-09 08:54:07 +0000

    非同期通信_修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,24 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
+    book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
     # comment = current_user.book_comments.new(book_comment_params)
     # サンプルコード（↑２行まとめると）
-    comment.book_id = @book.id
-    comment.save
+    @comment.book_id = book.id
+    @comment.save
     # redirect_to book_path(book)↓同じ画面に戻る
     # redirect_to request.referer
-    render index
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
     #上サンプルコード採用：要復習
     # BookComment.find(params[:id]).destroy
     @book = Book.find(params[:book_id])
     # redirect_to book_path(params[:book_id])↓同じ画面に戻る
     # redirect_to request.referer
-    render index
   end
 
   private
