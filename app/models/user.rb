d975cf07d589aec974dc44f886434bb4bc8952db
NoriakiNakamura <babababargxx@gmail.com>
2022-02-15 04:57:37 +0000

    final_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
-  #以下サンプルコード
-  # # 自分がフォローされる（被フォロー）側の関係性
-  # has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # # 被フォロー関係を通じて参照→自分をフォローしている人
-  # has_many :followers, through: :reverse_of_relationships, source: :follower
 
-  # # 自分がフォローする（与フォロー）側の関係性
-  # has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # # 与フォロー関係を通じて参照→自分がフォローしている人
-  # has_many :followings, through: :relationships, source: :followed
-# ------------------------------------------------------------------------
 
   has_one_attached :profile_image
 
